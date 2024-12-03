3080bca975a3e260cca067f21417545c7615d749
Matvei <pppprtoyy@gmail.com>
2024-12-03 22:16:17 +0300

    task_2 almost ready

--- All_Tasks/func_2.cpp
+++ All_Tasks/func_2.cpp
@@ -1,6 +1,51 @@
 #include <func_1.h>
 #include <func_2.h>
 
+
+std::string addBinary(std::string binaryFirst, std::string binarySecond) {
+    if (binarySecond.length() > binaryFirst.length()) {
+        std::swap(binaryFirst, binarySecond);
+    }
+
+    std::string result(binaryFirst.length() + 1, '0');
+    int carry = 0;
+    int j = binarySecond.length() - 1;
+
+    for (int i = binaryFirst.length() - 1; i >= 0; --i) {
+        int sum = carry + (binaryFirst[i] - '0');
+        if (j >= 0) {
+            sum += (binarySecond[j] - '0');
+            --j;
+        }
+        carry = sum / 2;
+        result[i + 1] = (sum % 2) + '0';
+    }
+
+    result[0] = carry + '0';
+    return result;
+}
+
+std::string sumOfBinary(long long first, long long second){
+
+    std::string binaryFirst = AnyNumberToReverse(first);
+    std::string binarySecond = AnyNumberToReverse(second);
+
+    if (binarySecond.length() >  binaryFirst.length()) {
+        std::swap(binaryFirst, binarySecond);
+    }
+    std::string result = addBinary(binaryFirst, binarySecond);
+    
+    return result;
+}
+// сделать блин функцию выше чтобы она блять числа принимала двочиные и допиоить потом сделаитть перевод в десятичную 
+std::string checkAndAddOne(std::string binary) { 
+        if (!binary.empty() && binary[0] == '1') { 
+            std::string one = "1"; 
+            return sumOfBinary(binary, one); 
+            } 
+            return binary;
+        }
+
 void ExplainTask2() {
     std::cout << "Perform the addition of numbers as per the given instructions. Develop functions to perform the addition operation.\n"
               << "Ensure that the program handles both positive and negative numbers. The length of the input numbers is not limited.\n"
@@ -15,11 +60,15 @@ void InputValues2(){
     inputCheck(first);
     std::cout << "Please enter a second integer number: ";
     inputCheck(second);
-;
-    std::string binaryFirst = AnyNumberToReverse(first);
-    std::string binarySecond = AnyNumberToReverse(second);
 
-    size_t size = std::max(binaryFirst.length(), binarySecond.length());
+    std::string result = sumOfBinary(first, second);
+    std::string checkAndAddOne(const std::string &binary) { 
+        if (!binary.empty() && binary[0] == '1') { 
+            std::string one = "1"; 
+            return addBinaryStrings(binary, one); 
+            } 
+            return binary;
+        }
+    
 
-    std::string result = 
 }
